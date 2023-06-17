44760da112b10bfb10a6d3fd801963bdfaac03f2
eric polley <ericpolley@live.com>
2023-06-17 09:19:30 -0700

    home buttons graphics

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,25 +23,33 @@ export default function Home() {
           {/* ICON */}
           <div class="flex-col">
       <Link to="/animation">
-        <img src="https://cdn0.iconfinder.com/data/icons/work-from-home-and-jobs-outline-style/512/animation-cartoon-design-character-Animator-3-256.png"
-        className='lg:w-32 lg:h-32 sm:w-28 w-16 h-auto object-cover object-center hover:bg-orange-400 rounded-full cursor-pointer  border-4 border-t-8 border-black'
+        <img src="https://github.com/ericpolley/flashdashdesign/blob/main/src/images/1motion.png?raw=true"
+        className='lg:w-32 lg:h-32 sm:w-28 w-16 h-auto object-cover object-center hover:bg-orange-400 rounded-full cursor-pointer  border-4 border-t-8 border-orange-600'
          alt="Linkedin"></img></Link>
          {/* ICON End */}
          <p className=" lg-text-xl md-text-sm  font-semibold">Animation</p> </div>
+         {/* ICON */}
+         <div class="flex-col">
+      <Link to="/graphics">
+        <img src="https://github.com/ericpolley/flashdashdesign/blob/main/src/images/1art.png?raw=true"
+        className='lg:w-32 lg:h-32 sm:w-28 w-16 h-auto object-cover object-center hover:bg-orange-400 rounded-full cursor-pointer  border-4 border-t-8 border-orange-600'
+         alt="Linkedin"></img></Link>
+         {/* ICON End */}
+         <p className=" lg-text-xl md-text-sm  font-semibold">Illustration</p> </div>
                           {/* ICON */}
       {/* ICON */}
       <div class="flex-col">
       <Link to="/game">
-        <img src="https://cdn4.iconfinder.com/data/icons/ionicons/512/icon-game-controller-b-256.png"
-        className='lg:w-32 lg:h-32 sm:w-28 w-16 h-auto object-cover hover:bg-orange-400 rounded-full cursor-pointer border-4 border-t-8 border-black'
+        <img src="https://github.com/ericpolley/flashdashdesign/blob/main/src/images/1game.png?raw=true"
+        className='lg:w-32 lg:h-32 sm:w-28 w-16 h-auto object-cover hover:bg-orange-400 rounded-full cursor-pointer border-4 border-t-8 border-orange-600'
          alt="Linkedin"></img></Link>
          {/* ICON End */}
          <p className=" lg-text-xl md-text-sm  font-semibold">Games</p> </div>
          {/* ICON */}
          <div class="flex-col">
       <Link to="/software">
-        <img src="https://cdn1.iconfinder.com/data/icons/jumpicon-marketing-glyph/32/-_Web-Page-Internet-Website-Browser-Window-256.png"
-        className='lg:w-32 lg:h-32 sm:w-28 w-16 h-auto object-cover object-center hover:bg-orange-400 rounded-full cursor-pointer  border-4 border-t-8 border-black'
+        <img src="https://github.com/ericpolley/flashdashdesign/blob/main/src/images/1web.png?raw=true"
+        className='lg:w-32 lg:h-32 sm:w-28 w-16 h-auto object-cover object-center hover:bg-orange-400 rounded-full cursor-pointer  border-4 border-t-8 border-orange-600'
          alt="Linkedin"></img></Link>
          {/* ICON End */}
          <p className=" lg-text-xl md-text-sm font-semibold">Web Dev</p> </div>
@@ -49,16 +57,16 @@ export default function Home() {
          {/* ICON */}
          <div class="flex-col justify-center">
                                    <Link to="/blog">
-        <img src="https://cdn1.iconfinder.com/data/icons/internet-of-things-24/160/blog-256.png"
-        className='lg:w-32 lg:h-32 sm:w-28 w-16 h-auto object-cover object-center hover:bg-orange-400 rounded-full cursor-pointer  border-4 border-t-8 border-black'
+        <img src="https://github.com/ericpolley/flashdashdesign/blob/main/src/images/1blog.png?raw=true"
+        className='lg:w-32 lg:h-32 sm:w-28 w-16 h-auto object-cover object-center hover:bg-orange-400 rounded-full cursor-pointer  border-4 border-t-8 border-orange-600'
          alt="Linkedin"></img></Link>
          {/* ICON End */}
          <p className=" lg-text-xl md-text-sm  font-semibold">Blog</p> </div>
               {/* ICON */}   
                           <div class="flex-col">
       <Link to="/tutorials">
-        <img src="https://cdn1.iconfinder.com/data/icons/education-1-42/48/26-256.png"
-        className='lg:w-32 lg:h-32 sm:w-28 w-16 h-auto object-cover object-center hover:bg-orange-400 rounded-full cursor-pointer  border-4 border-t-8 border-black'
+        <img src="https://github.com/ericpolley/flashdashdesign/blob/main/src/images/1tutorial.png?raw=true"
+        className='lg:w-32 lg:h-32 sm:w-28 w-16 h-auto object-cover object-center hover:bg-orange-400 rounded-full cursor-pointer  border-4 border-t-8 border-orange-600'
          alt="Linkedin"></img></Link>
          {/* ICON End */}
          <p className=" lg-text-xl md-text-sm  font-semibold">Tutorials</p> </div>
