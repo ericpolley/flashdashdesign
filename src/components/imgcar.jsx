468cc208b8a6b7efd4297159b3efdcef5252146d
eric polley <ericpolley@live.com>
2023-06-23 14:30:55 -0700

    new images to carousel

--- src/components/imgcar.jsx
+++ src/components/imgcar.jsx
@@ -3,14 +3,17 @@ import ImageCarousel from './ImageCarousel';
 
 const ImgCar = () => {
   const images = [
+    'https://github.com/ericpolley/flashdashdesign/blob/main/src/images/tylers%20tires%200001.png?raw=true',
+    'https://github.com/ericpolley/flashdashdesign/blob/main/src/images/Blue%20lawncare%20001.png?raw=true',
     'https://github.com/ericpolley/flashdashdesign/blob/main/src/images/paintbrush%20painting%20co%20001.png?raw=true',
     'https://github.com/ericpolley/flashdashdesign/blob/main/src/images/leanGreenAirducts%20001.png?raw=true',
-    'https://github.com/ericpolley/flashdashdesign/blob/main/src/images/paintbrush%20painting%20co%20001.png?raw=true',
     'https://github.com/ericpolley/flashdashdesign/blob/main/src/images/lighting%20Logo%20001.png?raw=true',
+    
+
   ];
 
   return (
-    <div className=" p-8 m-2">
+    <div className=" m-1">
       
       <ImageCarousel images={images} />
     </div>
